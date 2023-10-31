ce0c327b9f5d019b2f184251a311375d586e62e0
Justin Byrd <justinbyrd7@gmail.com>
2023-10-31 16:08:06 -0500

    episode test complete

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Display from './../Display';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./../Display";
 
-test('renders without errors with no props', async () => { });
+test("renders without errors with no props", async () => {});
 
-test('renders Show component when the button is clicked ', () => { });
+test("renders Show component when the button is clicked ", () => {});
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test("renders show season options matching your data when the button is clicked", () => {});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,26 +3,59 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
-test("renders without error", () => {});
-
-test("renders the summary test passed as prop", () => {});
-
-test("renders default image when image is not defined", () => {});
-
 // ----- EXAMPLE EPISODE TEST OBJECT -----
-// const exampleEpisodeData = {
-//   airdate: "2016-07-15",
-//   airstamp: "2016-07-15T12:00:00+00:00",
-//   airtime: "",
-//   id: 553946,
-//   image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
-//   name: "Chapter One: The Vanishing of Will Byers",
-//   number: 1,
-//   rating: { average: 8.2 },
-//   runtime: 49,
-//   season: 1,
-//   summary:
-//     "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
-//   type: "regular",
-//   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-// };
+const testData = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  image:
+    "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
+  name: "Chapter One: The Vanishing of Will Byers",
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+};
+
+const testDataWithoutImage = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  image: null,
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+};
+
+test("renders without error", () => {
+  render(<Episode episode={testData} />);
+});
+
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testData} />);
+  const summary = screen.queryByText(
+    /A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest./i
+  );
+  expect(summary).toBeInTheDocument;
+  expect(summary).toBeTruthy;
+  expect(summary).toHaveTextContent(
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest."
+  );
+});
+
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testDataWithoutImage} />);
+  const image = screen.queryByAltText("./stranger_things.png");
+  expect(image).toBeInTheDocument;
+});
